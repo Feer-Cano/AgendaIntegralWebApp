3607e92e81c3b4e272bae232f66682537045f462
Baraju2 <122385981+Baraju2@users.noreply.github.com>
2023-05-27 10:37:51 -0600

    Se depura el proyecto y se crea la ramificación del sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
+import { NotfoundComponent } from './dashboard/components/notfound/notfound.component';
+import { ProductService } from './dashboard/services/product.service';
+import { CountryService } from './dashboard/services/country.service';
+import { CustomerService } from './dashboard/services/customer.service';
+import { EventService } from './dashboard/services/event.service';
+import { IconService } from './dashboard/services/icon.service';
+import { NodeService } from './dashboard/services/node.service';
+import { PhotoService } from './dashboard/services/photo.service';
 
 @NgModule({
     declarations: [
