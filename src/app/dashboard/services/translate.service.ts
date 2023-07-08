e4488e9a4f6f4dacef0f0f7d6cc8768cfab07cc0
JoseCordero123 <aux.sistemas@fertilcenter.com.mx>
2023-07-08 12:09:34 -0600

    Creacion-de-archivos-JSON-en-base-a-las-normas
    se hicieron pruebas unitarias y todo esta funcionando

--- src/app/dashboard/services/translate.service.ts
+++ src/app/dashboard/services/translate.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import * as translateData from './translate.json';
-import * as inglesData from './ingles.json';
+import * as translateData from '../../../assets/I18N/I18N_ES.json';
+import * as inglesData from '../../../assets/I18N/I18N_EN.json';
 type TranslateData = {
   [key: string]: string;
 };
