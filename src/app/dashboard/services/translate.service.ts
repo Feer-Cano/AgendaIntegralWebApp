dcbbe9708d785ec7c2604ec3c8ef275eeb2bc24b
JoseCordero123 <aux.sistemas@fertilcenter.com.mx>
2023-07-08 12:29:57 -0600

    Cambio-de-nombres-de-archivos-JSON
    se cambiaron los nombres

--- src/app/dashboard/services/translate.service.ts
+++ src/app/dashboard/services/translate.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import * as translateData from '../../../assets/I18N/I18N_ES.json';
-import * as inglesData from '../../../assets/I18N/I18N_EN.json';
+import * as translateData from '../../../assets/i18n/i18n-es.json';
+import * as inglesData from '../../../assets/i18n/i18n-en.json';
 type TranslateData = {
   [key: string]: string;
 };
